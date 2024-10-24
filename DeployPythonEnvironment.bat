abd374d4317183bb7809551c1611602d4d25fda2
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-10-24 21:07:35 +0800

    Add files via upload

---
+++ DeployPythonEnvironment.bat
@@ -0,0 +1,6 @@
+@echo off
+
+curl -o DeployPythonEnvironment.zip https://codeload.github.com/Wangbo-An/DeployPythonEnvironment/zip/refs/heads/main
+tar -xvzf DeployPythonEnvironment.zip
+
+del DeployPythonEnvironment.zip

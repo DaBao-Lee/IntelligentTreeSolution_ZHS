39c48d35d8ba34b3a06b843c2053340b35a74a05
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-10-26 13:01:12 +0800

    Delete DeployPythonEnvironment.bat

--- DeployPythonEnvironment.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-curl -o DeployPythonEnvironment.zip https://codeload.github.com/Wangbo-An/DeployPythonEnvironment/zip/refs/heads/main
-tar -xvzf DeployPythonEnvironment.zip
-
-del DeployPythonEnvironment.zip

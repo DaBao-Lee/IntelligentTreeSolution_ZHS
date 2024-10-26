20155badd190aacb7c48ead4fb9065e41477902f
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-10-26 18:57:36 +0800

    Add files via upload

--- question.py
+++ question.py
@@ -15,7 +15,7 @@ class questMoudle:
         self.index = 0
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
-        self.ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+        self.ocr = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True)
         self.js = None
         
     def startAnswer(self):
@@ -23,17 +23,18 @@ class questMoudle:
         print("答题模式".center(60, '-'))
         if self.driver.current_url != "https://onlineweb.zhihuishu.com/onlinestuh5":
             self.driver.get("https://onlineweb.zhihuishu.com/onlinestuh5")
+        time.sleep(1.5)
         courseName = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'courseName')))[self.index].text
-        time.sleep(0.5)
         if os.path.exists(f"data/{courseName}.json"):
             self.js = json.load(open(f"data/{courseName}.json", encoding="utf-8"))
             print(courseName + "题库已加载完成")
             rightItem = self.driver.find_elements(By.CLASS_NAME, 'right-item-course')[self.index]
             rightItem.find_elements(By.CLASS_NAME, "course-menu-w")[1].click()
-            time.sleep(1.5)
+            time.sleep(2)
             self.driver.switch_to.window(self.driver.window_handles[-1])
             tmp_url = self.driver.current_url
             ok = self.wait.until(EC.presence_of_element_located((By.ID, 'examStateTabWsj')))
+            time.sleep(2)
             charpters = self.driver.find_elements(By.CLASS_NAME, 'examItemWrap')
             charpters = [x for x in charpters if x.find_element(By.CLASS_NAME, 'percentage_number').text in ["作业", "见面课测验"]]
             if len(charpters) == 0: print("所有见面课或单元测试均已完成.")
@@ -44,15 +45,14 @@ class questMoudle:
                 charpters = self.driver.find_elements(By.CLASS_NAME, 'examItemWrap')
                 charpters = [x for x in charpters if x.find_element(By.CLASS_NAME, 'percentage_number').text in ["作业", "见面课测验"]]
                 charpters[index].find_element(By.CLASS_NAME, "themeBg").click()
-                time.sleep(1.5)
+                time.sleep(3)
                 self.driver.switch_to.window(self.driver.window_handles[-1])
-                time.sleep(1.5)
                 problems = self.driver.find_elements(By.CLASS_NAME, "subject_type_describe")
                 for index in range(len(problems)):
                     img_btyes = problems[index].screenshot_as_png
                     img = cv2.imdecode(np.frombuffer(img_btyes, np.uint8), 1)
                     results = self.ocr.ocr(img)
-                    time.sleep(1)
+                    time.sleep(0.8)
                     txt = ''
                     for result in results[0][1: ]:
                         txt += result[-1][0]
@@ -63,7 +63,7 @@ class questMoudle:
                     answers = answers_box.find_elements(By.CLASS_NAME, 'nodeLab')
                     answers_dic = {v:k for k,v in enumerate([x.text for x in answers])}
                     if type(true_answer) == str:
-                        arg = self.similarityCalc(true_answer[2: ], answers_dic)
+                        arg = self.similarityCalc(true_answer, answers_dic)
                         answers[arg].click()
                     elif type(true_answer) == int:
                         if true_answer == 0:
@@ -75,11 +75,20 @@ class questMoudle:
                     else:
                         args = []
                         for item in true_answer:
-                            args.append(self.similarityCalc(item[2: ], answers_dic))
+                            args.append(self.similarityCalc(item, answers_dic))
                         for arg in args:
                             answers[arg].click()
                     self.driver.find_elements(By.CLASS_NAME, 'el-button--primary')[-1].click()
-                    time.sleep(1)
+                    time.sleep(0.5)
+                    self.driver.find_element(By.CLASS_NAME, 'btnStyleXSumit').click()
+                    time.sleep(0.5)
+                    self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[3]/button[2]').click()
+                    time.sleep(0.9)
+                    windows = self.driver.window_handles
+                    for window in windows[: -1]:
+                        self.driver.switch_to.window(window)
+                        time.sleep(0.3)
+                        self.driver.close()
         else:
             print("暂未有该门课程答案 停止作答.")
         self.index += 1

b25a82bb2007a5c73b48b1fa618269585b3352f7
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-11-01 20:35:05 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,9 +1,20 @@
 @echo off
 
 color E
-
+echo 正在初始化......
 pip install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple --user -q
 pip install onnxruntime onnx onnxslim opencv-python selenium paddlepaddle paddleocr  fuzzywuzzy -i https://pypi.tuna.tsinghua.edu.cn/simple -q
+
+cd data
+if exist answers.rar (
+ 	 tar -xvzf answers.rar
+	echo 题库初始化完成
+	del answers.rar
+) else (
+	 echo 
+)
+
+cd ../
 echo ****************************************************************************
 echo The pickles needed have already installed...
 echo Start run the script...

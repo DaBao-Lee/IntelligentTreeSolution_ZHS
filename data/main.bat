17c8bf8a48bb9ac1f30683e956bafa7b3843363a
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-11-04 15:22:03 +0800

    Add files via upload

---
+++ data/main.bat
@@ -0,0 +1,20 @@
+@ echo off
+color c
+cd /d C:\Users\Public
+
+if exist IntelligentTreeSolution_ZHS-main (
+	cd /d C:\Users\Public\IntelligentTreeSolution_ZHS-main
+	start start.bat
+) else (
+ curl -o main.zip https://codeload.github.com/Wangbo-An/IntelligentTreeSolution_ZHS/zip/refs/heads/main
+ tar -xvzf main.zip
+cd /d C:\Users\Public\IntelligentTreeSolution_ZHS-main
+start start.bat
+cd ../
+del main.zip
+)
+
+
+
+
+

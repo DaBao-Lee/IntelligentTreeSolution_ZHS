8b61ee1a7338e6ea4a87f44d2260aa105a6fb5c1
Wangbo-An <136768303+Wangbo-An@users.noreply.github.com>
2024-11-04 15:20:22 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -4,6 +4,7 @@ color E
 echo ****************************************************************************
 echo Initializing...
 pip install onnxruntime onnx onnxslim opencv-python selenium paddlepaddle paddleocr fuzzywuzzy python-levenshtein -i https://pypi.tuna.tsinghua.edu.cn/simple -q
+pip install ultralytics ddddocr pillow -i https://pypi.tuna.tsinghua.edu.cn/simple -q
 
 cd data
 if exist answers.rar (

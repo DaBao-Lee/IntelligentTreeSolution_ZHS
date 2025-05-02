533a00f7599a564f59449f6d517c04f12c42c142
DaBao-Lee <1760846142@qq.com>
2025-05-02 16:42:19 +0800

    Add files via upload

--- PythonEnvironmentDeploy/DeployPythonEnvironment.bat
+++ PythonEnvironmentDeploy/DeployPythonEnvironment.bat
@@ -1,6 +1,6 @@
 @echo off
 
-curl -o DeployPythonEnvironment.zip https://codeload.github.com/Wangbo-An/DeployPythonEnvironment/zip/refs/heads/main
+curl -o DeployPythonEnvironment.ziphttps://codeload.github.com/DaBao-Lee/DeployPythonEnvironment/zip/refs/heads/main
 tar -xvzf DeployPythonEnvironment.zip
 
 del DeployPythonEnvironment.zip

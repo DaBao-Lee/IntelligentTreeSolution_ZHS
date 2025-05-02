a29ede171b331d1ae7af7dc7070553ac506f2e98
DaBao-Lee <1760846142@qq.com>
2025-05-02 16:41:29 +0800

    Add files via upload

--- main.bat
+++ main.bat
@@ -6,7 +6,7 @@ if exist IntelligentTreeSolution_ZHS-main (
 	cd /d C:\Users\Public\IntelligentTreeSolution_ZHS-main
 	start start.bat
 ) else (
- curl -o main.zip https://codeload.github.com/Wangbo-An/IntelligentTreeSolution_ZHS/zip/refs/heads/main
+ curl -o main.zip https://codeload.github.com/DaBao-Lee/IntelligentTreeSolution_ZHS/zip/refs/heads/main
  tar -xvzf main.zip
 cd /d C:\Users\Public\IntelligentTreeSolution_ZHS-main
 start start.bat
